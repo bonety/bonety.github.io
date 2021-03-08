6bfb19b79b4cdd2393d7068e19897ac219ef61f6
unknown <nikolas_riedel@web.de>
2021-03-08 23:06:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var app = new Vue({
     data: {
       fractals: 0
     },
-    // define methods under the `methods` object
     methods: {
         inc: function (event) {
         this.fractals += 1;
