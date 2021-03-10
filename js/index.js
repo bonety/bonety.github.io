8da9f493ed79c21fea9e5d944f91469cc0e48527
unknown <nikolas_riedel@web.de>
2021-03-10 23:36:21 +0100

    first fractal
    first fractal. not sure about the way of making the fractal yet. chaos

--- js/index.js
+++ js/index.js
@@ -12,14 +12,44 @@ var app = new Vue({
       var canvas = document.getElementById("fractalDraw");
       if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
-        ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect(a,a,a,a);
-        b = a;
-        if (b > 150){
-          b = 300 - a;
+
+        // Coordinates of the shape's points 
+        const shape = [
+          { x: 250, y: 50 },
+          { x: 450, y: 450 },
+          { x: 50, y: 450 },
+        ];
+        
+        // Coordinates of a random point
+        let point = {
+          x: Math.round(Math.random() * 500),
+          y: Math.round(Math.random() * 500),
+        };
+
+        // How many points we've drawn so far
+        let count = 0;
+
+        while (count < 15000) {
+          // Pick a random number: 0, 1, or 2
+          let rand = Math.floor(Math.random() * shape.length);
+
+          // Select a corner based on the random number
+          let corner = shape[rand];
+
+          // Compute coordinates, midway between 'point' and 'corner'
+          point.x = (point.x + corner.x) / 2;
+          point.y = (point.y + corner.y) / 2;
+
+          // Draw the new point
+          ctx.fillStyle = "rgba(200,0,0)";
+          ctx.fillRect(point.x, point.y, 1, 1);
+          ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+          ctx.fillRect(point.x + a, point.y + a, 1, 1);
+
+          // Increment 'count', once it reaches 15000 we stop the loop
+          count++;
         }
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect(20 + b, 20 + b, 20 + b , 20 + b);
+        
       }
     }
   },
--- js/player.js
+++ js/player.js
@@ -1,4 +1,16 @@
 var player = {
-    fractals: 0
+    fractals: 0,
+    generators: []
 }
+
+var firstGenerator = {
+    cost: 20,
+    mult: 1,
+    amount: 0,
+    bought: 0
+}
+
+player.generators.push(firstGenerator)
+
+
 
